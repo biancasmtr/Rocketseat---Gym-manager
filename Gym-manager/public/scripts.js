56e6659061d51f4c067413566bf1268258fd0007
Bi <biancasouzamonteiro@gmail.com>
2020-07-21 10:04:03 -0300

    Add files via upload
    Project Gymanager finalizado.

---
+++ Gym-manager/public/scripts.js
@@ -0,0 +1,66 @@
+const { create } = require("../src/app/models/member");
+
+const currentPage = location.pathname;
+const menuItens = document.querySelectorAll("header .links a");
+
+for (item of menuItens) {
+  if (currentPage.includes(item.getAttribute("href"))) {
+    item.classList.add("active");
+  }
+}
+
+
+// Paginação
+
+function paginate(selectedPages, totalPages) {
+  let pages = [],
+      oldPage 
+
+  for (let currentPage = 1; currentPage <= totalPages; currentPage++){
+
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const pagesBeforeSelectedPage = currentPage >= selectedPages - 2
+    const pagesAfterSelectedPage = currentPage <= selectedPages + 2
+
+    if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+      if (oldPage && currentPage - oldPage > 2) {
+        pages.push("...")
+      }
+      if (oldPage && currentPage - oldPage == 2) {
+        pages.push(oldPage + 1)
+      }
+
+      pages.push(currentPage)
+      oldPage = currentPage
+    }
+  }
+
+  return pages
+}
+
+function createPagination(pagination) {
+  const page = +pagination.dataset.page;
+  const total = +pagination.dataset.total;
+  const filter = pagination.dataset.filter;
+  
+  const pages = paginate(page, total)
+  
+  let elements = ""
+  
+  for (let page of pages ) {
+    if (String(page).includes("...")) {
+      elements += `<span>${page}</span>`
+    } else {
+      if (filter) {
+        elements += `<a href="?page=${page}&filter=${filter}">${page}</a>` 
+      }
+      elements += `<a href="?page=${page}">${page}</a>`  
+    }
+  }
+  pagination.innerHTML = elements
+}
+
+const pagination = document.querySelector(".pagination")
+if (pagination) {
+  createPagination(pagination)
+}
